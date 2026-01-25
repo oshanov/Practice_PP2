5313d83a526c0601e35baf8e5a14b5815d73d4c2
Mukhamedali <m_oshanov@kbtu.kz>
2026-01-25 22:49:39 +0600

    Final Commit

--- Practice1/python-basics/data types/data_types.py
+++ Practice1/python-basics/data types/data_types.py
@@ -1,10 +1,3 @@
-fruits = list(('apple', 'banana', 'cherry'))
-print('I`ve created list of fruits. It`s data type is', end=" ")
-print(type(fruits))
-
-a, b, c = fruits
-
-print('Here we go:', a, b, c)
-
-print('Now, single fruits have data type:', end=" ")
-print(type(a), type(b), type(c))
+x = "Hello, World!"
+print("I can see which data type x has using type()")
+print(type(x))
--- Practice1/python-basics/variables/hbjhbj.py
+++ Practice1/python-basics/variables/hbjhbj.py
@@ -1,2 +1,10 @@
-nam = input()
-print("Hello,", nam + "!")
+x = 25 #Defining variable
+y = x #Def. sec. var. same as first
+print(x, y)
+x = float(21) #Giving variable another value
+print(x)
+
+#Also I can give non-numeric value to existing var.
+x = str("Hello, World!")
+print()
+print(x)
