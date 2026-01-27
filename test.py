0a9ba6cf85621cd0e409789ff1389ccda49cbed5
Mukhamedali <m_oshanov@kbtu.kz>
2026-01-27 15:52:04 +0600

    Testing

---
+++ test.py
@@ -0,0 +1,16 @@
+# a = [5,4,3,2,1]
+# for i in a:
+#     print(i)
+
+# a.append(23)
+# print(a)
+
+# a.pop(0)
+# print(a)
+
+# print(len(a))
+# /////////////////////////
+# s = 'Hello'
+# for i in range(len(s)):
+#     print(s[i])
+# ////////////////////////
---
+++ test2.py
@@ -0,0 +1,4 @@
+friends = ['Didar', 'Asanali', 'Abdyrakhman']
+for friend in friends:
+    print(f'Happy New Year, {friend}!')
+print("Blast!!!")
